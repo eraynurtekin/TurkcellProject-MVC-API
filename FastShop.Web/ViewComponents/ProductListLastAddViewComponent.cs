d38689c90bee565725d932f41fc9e17855d2c70b
Eray Nurtekin <79145084+eraynurtekin@users.noreply.github.com>
2022-05-05 16:22:21 +0300

    Son eklenen ürünleri listeleme eklendi.

---
+++ FastShop.Web/ViewComponents/ProductListLastAddViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using FastShop.Business;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FastShop.Web.ViewComponents
+{
+    public class ProductListLastAddViewComponent : ViewComponent
+    {
+        private readonly IProductService productService;
+
+        public ProductListLastAddViewComponent(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var products = productService.GetAllProductsOrderBy().Take(5);
+            return View(products);
+
+        }
+    }
+}
